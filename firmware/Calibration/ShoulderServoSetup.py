1ba11f88f24bb3a04e88f5d03fdf28bdd849638f
Dakshesh Nankani <daksheshnankani1405@gmail.com>
2025-07-18 10:38:15 +0530

    finished all join setups

---
+++ firmware/Calibration/ShoulderServoSetup.py
@@ -0,0 +1,16 @@
+from time import sleep
+from adafruit_servokit import ServoKit
+
+kit = ServoKit(channels = 16)
+
+kit.servo[5].angle = 0
+print("set angle to 0")
+sleep(3)
+
+kit.servo[5].angle = 90
+print("set angle to 90")
+sleep(3)
+
+kit.servo[5].angle = 180
+print("set angle to 180")
+sleep(3)
---
+++ firmware/Calibration/lowerForeArmServoSetup.py
@@ -0,0 +1,21 @@
+from time import sleep
+from adafruit_servokit import ServoKit
+
+kit = ServoKit(channels = 16)
+
+kit.servo[2].angle = 0
+print("set servo to 0")
+sleep(3)
+
+kit.servo[2].angle = 90
+print("set servo to 90")
+sleep(3)
+
+kit.servo[2].angle = 180
+print("set servo to 180")
+sleep(3)
+
+while True:
+    kit.servo[2].angle = 90
+    print('indefinitely set angle to 90')
+    sleep(3)
