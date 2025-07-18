efeee66a98f496c73c5b101c9cda076867d33bc5
IronMan1405 <daksheshnankani1405@gmail.com>
2025-07-18 21:43:52 +0530

    added to style.css, and added live angle status

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from servo_controller import setServoAngle
 
 app = Flask(__name__)
 
+currentAngles = {0: 95, 1: 90}
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -12,7 +14,8 @@ def move():
     servo = int(request.form['servo'])
     angle = int(request.form['angle'])
     setServoAngle(servo, angle)
-    return ('', 204)
+    currentAngles[servo] = angle
+    return render_template('index.html', angles = currentAngles)
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 5000, debug = True)
